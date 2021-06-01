cc3b29290e1e75bd9c53f87a1dda8cb487c27a6e
cakrapangukir <84928124+cakrapangukir@users.noreply.github.com>
2021-06-01 18:52:34 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,27 +6,53 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1831824739:AAGpuXSFmVHLLhk-4phEjFlLVlhGRa8fKTs'
+const token = '1812069205:AAGRYRfWv6HOqDWdJTqxB786nNftAFmGI80'
 const bot = new TelegramBot(token, {polling: true});
 
-
-// bots
+// Main menu bot
 bot.onText(/\/start/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+// input requires i and r
+state = 0;
+bot.onText(/\/predict/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
-    );   
+        `masukan nilai i|v contohnya 9|9`
+    );  
+    state = 1;
 });
+
+bot.on('message', (msg) => { 
+    if(state == 1){
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang di prediksi adalah ${jres[0]} volt`
+            );
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang di prediksi adalah ${jres[1]} watt`
+            );
+       })
+   }else{
+       state = 0
+   }
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
